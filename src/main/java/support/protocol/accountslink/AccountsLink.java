9bcf69db3029eb70bea83582c705da8f882aa166
Artuto <artutogamer@gmail.com>
2019-01-19 21:31:32 -0600

    initial commit

---
+++ src/main/java/support/protocol/accountslink/AccountsLink.java
@@ -0,0 +1,19 @@
+package support.protocol.accountslink;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class AccountsLink extends Plugin
+{
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
